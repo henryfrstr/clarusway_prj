1db92cef98c5228f0b4e779bae4f22e161090194
henryfrstr <63148122+henryfrstr@users.noreply.github.com>
2020-12-24 22:50:42 +0300

    added models

--- clarusway/fscohort/views.py
+++ clarusway/fscohort/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def home_view(request):
     return HttpResponse("Hi, this is fscohort Home page.")
+
+
+def about_view(request):
+    return HttpResponse("Hi ,this is fscohort about page.")
